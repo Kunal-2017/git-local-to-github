5d0b584db4955ff06e31778e70ab7920d85dc12c
Kunal-2017 <106303693+Kunal-2017@users.noreply.github.com>
2024-09-16 16:32:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import AddTodo from './components/AddTodo/AddTodo'
+
+import TodoList from './components/TodoList/TodoList'
+
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addTodo, deleteTodo, editTodo, finishTodo} from './slice/TodoSlice';
+function App() {
+  const dispatch = useDispatch();
+  const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, finishTodo}, dispatch);
+  return (
+   <>
+        <AddTodo addTodo={actions.addTodo} />
+        <TodoList deleteTodo={actions.deleteTodo} editTodo={actions.editTodo} finishTodo={actions.finishTodo} />
+   </>
+  )
+}
+
+export default App;
